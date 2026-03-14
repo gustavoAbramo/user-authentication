5bb284b6eab7ec3142787055d05fc2afb021a8ea
Coffee-Gutts <gustavoabramooo@gmail.com>
2026-03-14 00:04:27 -0300

    feat: add hash password

--- auth-api/src/main/java/com/example/auth_api/config/SecurityConfig.java
+++ auth-api/src/main/java/com/example/auth_api/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
@@ -20,5 +22,9 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- auth-api/src/main/java/com/example/auth_api/service/UserService.java
+++ auth-api/src/main/java/com/example/auth_api/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.auth_api.service;
 
 import com.example.auth_api.entity.User;
 import com.example.auth_api.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,12 +11,17 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder encoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder encoder) {
         this.repository = repository;
+        this.encoder = encoder;
     }
 
     public User createUser(User user) {
+        String encodedPassword = encoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
         return repository.save(user);
     }
 
